7826953ed63418c60962b90b25ae6f5280173f9e
Samuel Rounce <me@samuelrounce.co.uk>
2026-05-25 01:32:44 +0100

    feat: Update Winecarte to v0.4.0-alpha.3

--- install.sh
+++ install.sh
@@ -268,7 +268,7 @@ install_winecarte() {
   fi
 
   mkdir "${workdir}/winecarte"
-  curl -sL --fail "https://api.github.com/repos/srounce/winecarte/releases/328366058" \
+  curl -sL --fail "https://api.github.com/repos/srounce/winecarte/releases/328602520" \
     | grep "browser_download_url" \
     | cut -d : -f 2,3 \
     | tr -d \" \
